197c30ad5ebb601c8228751069d628312c2578c4
Ricky Sophin <sophinricky@gmail.com>
2022-08-11 21:13:01 -0400

    thursday night

--- api/cart_items.js
+++ api/cart_items.js
@@ -2,14 +2,31 @@ const express = require("express");
 const router = express.Router();
 const { requireUser } = require("./utils")
 
-router.get("/", async (req, res, next) => {
+router.get("/", requireUser, async (req, res, next) => {
+    const user = req.user
+    
     try {
-        const allCartItems = await getAllCart_items();
-        res.send(allCartItems)
+        const order = await getCartOrderByUser(user.id)
+        const cartItems = await getCartItemsByOrderId(order.id)
+        
+        res.send(cartItems)
     } catch (error) {
         next(error)
     }
 });
+
+router.post("/", requireUser, async (req, res, next) => {
+    const { product_id } = req.body;
+    try {
+        const order = await getCartOrderByUser(req.user.id)
+        const addCart = await addItemToCart({
+            product_id,
+            order_id: order.id
+        })
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 module.exports = router;
