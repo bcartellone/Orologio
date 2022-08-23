4a52d50b1276ff3d95861d4b7f2f800f256b157c
Ricky Sophin <sophinricky@gmail.com>
2022-08-23 15:27:09 -0400

    quantity

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -5,6 +5,7 @@ const SingleProduct = ({token, isLoggedIn, setIsLoggedIn}) => {
     const { id } = useParams();
 
     const [singleProduct, setSingleProduct] = useState({})
+    const [quantity, setQuantity] = useState(0)
 
     useEffect(() => {
     const fetchSingleProduct = async () => {
@@ -23,6 +24,7 @@ const SingleProduct = ({token, isLoggedIn, setIsLoggedIn}) => {
 
     const handleSubmit = async () => {
         try {
+            console.log('this is the quantitiy', quantity)
             if (isLoggedIn) {
             const response = await fetch(`/api/cart_items`, {
                 method: 'POST',
@@ -33,10 +35,12 @@ const SingleProduct = ({token, isLoggedIn, setIsLoggedIn}) => {
                 },
                 body: JSON.stringify({
                         productId: singleProduct.id,
+                        quantity: quantity
                 })
             })
             const data = await response.json();
         } else {
+            console.log('this is the quantity', quantity)
             const response = await fetch(`/api/cart_items`, {
                 method: 'POST',
                 headers: {
@@ -44,6 +48,7 @@ const SingleProduct = ({token, isLoggedIn, setIsLoggedIn}) => {
                 },
                 body: JSON.stringify({
                         productId: singleProduct.id,
+                        quantity: quantity
                 })
             })
             const data = await response.json();
@@ -56,6 +61,11 @@ const SingleProduct = ({token, isLoggedIn, setIsLoggedIn}) => {
             console.log(error)
         }
     } 
+    
+    const handleChange = (e) => {
+        setQuantity(e.target.value)
+    }
+
     return (
         <div style={{ display: 'flex', justifyContent: 'space-evenly'}}>
             <div>
@@ -67,6 +77,18 @@ const SingleProduct = ({token, isLoggedIn, setIsLoggedIn}) => {
             </div>
             <div style={{ border: "1px solid black", boxShadow: "3px 3px gray", padding: "5px"}}>
                 <h1>${singleProduct.price}</h1>
+                <select value={quantity} onChange={handleChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
                 <button onClick={handleSubmit}>Add to Cart</button>
             </div>
         </div>
---
+++ src/components/Users.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+const Users = ({token}) => {
+    const [allUsers, setAllUsers] = useState([])
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch('/api/users',{
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                const data = await response.json();
+
+                console.log(data);
+                setAllUsers(data);
+            } catch (error) {
+                console.error(error);
+            }
+        
+        };
+
+        fetchUsers();
+        }
+    }, []);
+    return (
+        <div>
+            {
+                allUsers && token ? allUsers.map((user) => {
+                    return(
+                        <div style={{display: 'flex', flexDirection: 'column', border: '2px solid black', padding: '10px'}}>
+                            <p>Id: {user.id}</p>
+                            <p>Username: {user.username}</p>
+                            <p>roleId: {user.roleId}</p>
+                        </div>
+                    )
+                }) : <h1>User must have admin privelages to access users</h1>
+            }
+        </div>
+    )
+    
+};
+
+export default Users; 
