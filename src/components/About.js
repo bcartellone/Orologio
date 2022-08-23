732119d9a17b4a253e0d7e04d394d2d0f4ae4a9a
bcartellone <46415199+bcartellone@users.noreply.github.com>
2022-08-23 11:56:02 -0400

    finished about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
 import './About.css';
+import adimage2 from './adimage2_aboutpage.jpeg';
 
 const About = () => {
     return (
         <div>
-            <h1 className='aboutBox'>This is our About Area</h1>
+            <div className='aboutContainer'>
+                <div className='aboutBox'>
+                    <h2> Since its creation is 2022, Cyborg Flying Watches has provided an unmatched experience to those looking for a luxury timepiece. We strive to provide customers with the highest quality of service in order to ensure you find the perfect piece, for the most special of occasions. </h2>
+                </div>
+                
+        </div>
+        <div className='imgContainer'>
+                 <div className='imgbox'>  
+                    <img className='imgsrc' src={adimage2} alt='image of a watch'/>  
+                </div>   
+        </div>
+
+        
+        <div className='emailContainer'>
+                <div className='emailBox'>
+                    <h3> Please contact us at: customercare@cyborgflying.com with questions or concerns. </h3>
+                </div>
+                
+        </div>
+
         </div>
     )
 };
